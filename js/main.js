c180ac55325d3d0001e607dcddb8ffc3a349fb0d
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-01-13 16:11:38 +0100

    esercizio completo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var active = 0;
 
 
-//
+//Immagine Successiva
 $('.fa-angle-right').click(function(){
   $('.images > img').eq(active).hide();
   if(active < 3){
@@ -9,5 +9,17 @@ $('.fa-angle-right').click(function(){
   } else{
     active = 0
   }
-  $('.images > img').eq(currentImage).fadeIn();
+  $('.images > img').eq(active).fadeIn();
+})
+
+
+//IMMAGINE PRECEDENTE
+$('.fa-angle-left').click(function(){
+  $('.images > img').eq(active).hide();
+  if(active > 0){
+    active --
+  } else{
+    active = 3
+  }
+  $('.images > img').eq(active).fadeIn();
 })
